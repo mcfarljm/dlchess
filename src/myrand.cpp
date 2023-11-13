77034edc5f2697138a6d1bc7329e198c9ac32c83
John McFarland <mcfarljm@gmail.com>
2023-11-12 17:01:38 -0700

    Add DirichletDistribution

--- src/myrand.cpp
+++ src/myrand.cpp
@@ -1,3 +1,18 @@
 #include "myrand.h"
 
 std::default_random_engine rng(std::random_device{}());
+
+std::vector<double> DirichletDistribution::sample() {
+  std::vector<double> samples;
+  double sum = 0.0;
+  for (int i=0; i<n; ++i) {
+    double sample = gamma_dist(rng);
+    sum += sample;
+    samples.push_back(sample);
+  }
+
+  for (auto& x : samples)
+    x /= sum;
+
+  return samples;
+}
--- src/myrand.h
+++ src/myrand.h
@@ -6,4 +6,17 @@
 extern std::default_random_engine rng;
 
 
+class DirichletDistribution {
+  double alpha;
+  int n;
+  std::gamma_distribution<> gamma_dist;
+
+public:
+  DirichletDistribution(int n, double alpha) :
+    n(n), alpha(alpha), gamma_dist(std::gamma_distribution<>(alpha)) {}
+
+  std::vector<double> sample();
+};
+
+
 #endif // MYRAND_H
