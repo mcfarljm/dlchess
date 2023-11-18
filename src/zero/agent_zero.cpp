3e2de4614cd54959f1f936f105a095094ce6df2d
John McFarland <mcfarljm@gmail.com>
2023-11-17 17:35:34 -0700

    Initial AlphaZero move selection agent
    Compiles, but not yet tested

---
+++ src/zero/agent_zero.cpp
@@ -0,0 +1,220 @@
+#include <algorithm>
+
+#include "agent_zero.h"
+#include "../myrand.h"
+
+
+namespace zero {
+
+  ZeroNode::ZeroNode(const board::Board& game_board, float value,
+                     std::unordered_map<Move, float, MoveHash> priors,
+                     std::weak_ptr<ZeroNode> parent,
+                     bool add_noise) :
+    game_board(game_board), value(value), parent(parent),
+    terminal(game_board.is_over()) {
+
+    for (const auto &[move, p] : priors) {
+      branches.emplace(move, p);
+    }
+
+    assert((! branches.empty()) || terminal);
+
+    if (add_noise && ! branches.empty()) {
+      // std::cout << "Orig prior: ";
+      // for (const auto &[move, p] : priors)
+      //   std::cout << move << " " << p << ", ";
+      // std::cout << std::endl;
+
+      // Sample noise on legal moves:
+      // Adjust concentration based on number of legal moves, following Katago
+      // paper.
+      double alpha = DIRICHLET_CONCENTRATION * 19.0 * 19.0 / branches.size();
+      auto dirichlet_dist = DirichletDistribution(branches.size(), alpha);
+      std::vector<double> noise = dirichlet_dist.sample();
+      // std::cout << "Noise: " << noise << std::endl;
+
+      size_t idx = 0;
+      for (auto &[move, prior]: priors) {
+        prior = (1.0 - DIRICHLET_WEIGHT) * prior +
+          DIRICHLET_WEIGHT * noise[idx];
+        ++idx;
+      }
+
+      // std::cout << "Noised prior: ";
+      // for (const auto &[move, p] : priors)
+      //   std::cout << move << " " << p << ", ";
+      // std::cout << std::endl;
+    }
+
+    if (terminal) {
+      // Override the model's value estimate with actual result
+      auto winner = game_board.winner().value();
+      if (game_board.side == winner)
+        ZeroNode::value = 1.0;
+      else if (game_board.side == Color::both)
+        ZeroNode::value = 0.0;
+      else
+        ZeroNode::value = -1.0;
+    }
+  }
+
+  void ZeroNode::record_visit(Move move, float value) {
+    ++total_visit_count;
+    auto it = branches.find(move);
+    assert(it != branches.end());
+    (it->second.visit_count)++;
+    (it->second.total_value) += value;
+  }
+
+
+  float ZeroNode::expected_value(Move m) const {
+    auto branch = branches.find(m)->second;
+    if (branch.visit_count == 0)
+      return 0.0;
+    return branch.total_value / branch.visit_count;
+  }
+
+
+  Move ZeroAgent::select_move(const board::Board& game_board) {
+    // std::cerr << "In select move, prior move count: " << game_state.num_moves << std::endl;
+    auto root = create_node(game_board);
+
+    for (auto round_number=0; round_number < num_rounds; ++round_number) {
+      // std::cout << "Round: " << round_number << std::endl;
+      auto node = root;
+      auto next_move = select_branch(*node);
+      // std::cout << "Selected root move: " << next_move << std::endl;
+      // for (auto it = node->children.find(next_move); it != node->children.end();) {
+      for (std::unordered_map<Move, std::shared_ptr<ZeroNode>, MoveHash>::const_iterator it;
+           it = node->children.find(next_move), it != node->children.end();) {
+        node = it->second;
+        if (node->terminal)
+          break;
+        next_move = select_branch(*node);
+      }
+
+      float value;
+      std::optional<Move> move;
+      if (! node->terminal) {
+        auto new_board = node->game_board;
+        new_board.make_move(next_move);
+        auto child_node = create_node(std::move(new_board), next_move, node);
+        value = -1 * child_node->value;
+        move = next_move;
+      }
+      else {
+        value = node->value;
+      }
+
+      while (node) {
+        if (node->terminal)
+          (node->total_visit_count)++;
+        else
+          node->record_visit(move.value(), value);
+        node = node->parent.lock();
+        value = -1 * value;
+      }
+    }
+
+    // if (collector) {
+    //   auto root_state_tensor = encoder->encode(game_state);
+    //   auto visit_counts = torch::zeros(encoder->num_moves());
+    //   auto get_visit_count = [&](int i) {
+    //     auto move = encoder->decode_move_index(i);
+    //     auto it = root->branches.find(move);
+    //     if (it != root->branches.end())
+    //       return it->second.visit_count;
+    //     else
+    //       return 0;
+    //   };
+    //   for (auto i=0; i < encoder->num_moves(); ++i) {
+    //     visit_counts.index_put_({i}, static_cast<float>(get_visit_count(i)));
+    //   }
+    //   collector->record_decision(root_state_tensor, visit_counts);
+    // }
+
+    if (greedy) {
+      // Select the move with the highest visit count
+      auto max_it = std::max_element(root->branches.begin(), root->branches.end(),
+                                     [&root] (const auto& p1, const auto& p2) {
+                                       return root->visit_count(p1.first) < root->visit_count(p2.first);
+                                     });
+
+      // for (const auto& [m, b] : root->branches)
+      //   std::cerr << "visits: " << m << " " << b.visit_count << std::endl;
+      // std::cerr << "E[V] = " << max_it->second.total_value / max_it->second.visit_count << ", visits = " << max_it->second.visit_count << std::endl;
+      return max_it->first;
+    }
+    else {
+      // Select move randomly in proportion to visit counts
+      std::vector<Move> moves;
+      std::vector<int> visit_counts;
+      for (const auto& [move, branch] : root->branches) {
+        moves.push_back(move);
+        visit_counts.push_back(root->visit_count(move));
+      }
+      std::discrete_distribution<> dist(visit_counts.begin(), visit_counts.end());
+      return moves[dist(rng)];
+    }
+  }
+
+
+
+  std::shared_ptr<ZeroNode> ZeroAgent::create_node(const board::Board& game_board,
+                                                   std::optional<Move> move,
+                                                   std::weak_ptr<ZeroNode> parent) {
+
+    // Note: also want to place this prior to loading jit model as well
+    c10::InferenceMode guard;
+
+    auto state_tensor = encoder->encode(game_board);
+    state_tensor.unsqueeze_(0);
+
+    std::vector<torch::jit::IValue> input({state_tensor});
+    auto output = model.forward(input);
+    auto priors = output.toTuple()->elements()[0].toTensor(); // Shape: (1, 8, 8, 73)
+    auto values = output.toTuple()->elements()[1].toTensor();
+
+    // std::cout << "priors from nn: " << priors << std::endl;
+
+    values.squeeze_();
+    priors.squeeze_();
+    // std::cout << "priors size: " << at::numel(priors) << std::endl;
+    auto value = values.item().toFloat();
+
+    auto move_coord_map = decode_legal_moves(game_board);
+    std::unordered_map<Move, float, MoveHash> move_priors;
+    for (const auto &[mv, coords] : move_coord_map) {
+      move_priors.emplace(mv, priors.index({coords}).item().toFloat());
+    }
+
+    auto new_node = std::make_shared<ZeroNode>(game_board, value,
+                                               std::move(move_priors),
+                                               parent,
+                                               ! parent.lock());
+    auto parent_shared = parent.lock();
+    if (parent_shared) {
+      assert(move);
+      parent_shared->add_child(move.value(), new_node);
+    }
+    return new_node;
+  }
+
+
+
+  Move ZeroAgent::select_branch(const ZeroNode& node) const {
+    auto score_branch = [&] (Move move) {
+      auto q = node.expected_value(move);
+      auto p = node.prior(move);
+      auto n = node.visit_count(move);
+      return q + c_uct * p * sqrt(node.total_visit_count) / (n + 1);
+    };
+    auto max_it = std::max_element(node.branches.begin(), node.branches.end(),
+                                   [score_branch] (const auto& p1, const auto& p2) {
+                                     return score_branch(p1.first) < score_branch(p2.first);
+                                   });
+    assert(max_it != node.branches.end());
+    return max_it->first;
+  }
+
+};
---
+++ src/zero/agent_zero.h
@@ -0,0 +1,106 @@
+#ifndef AGENT_ZERO_H
+#define AGENT_ZERO_H
+
+#include <memory>
+#include <optional>
+#include <unordered_map>
+#include <torch/script.h> // One-stop header.
+
+#include "encoder.h"
+/* #include "experience.h" */
+#include "../agent_base.h"
+
+namespace zero {
+
+  using namespace game_moves;
+
+  class Branch {
+  public:
+    float prior;
+    int visit_count = 0;
+    float total_value = 0.0;
+
+  public:
+    Branch(float prior) : prior(prior) {}
+  };
+
+
+  class ZeroNode {
+
+    // Concentration parameter for dirichlet noise:
+    constexpr static double DIRICHLET_CONCENTRATION = 0.03;
+    constexpr static float DIRICHLET_WEIGHT = 0.25;
+
+  public:
+    board::Board game_board;
+
+    std::weak_ptr<ZeroNode> parent;
+    std::unordered_map<Move, std::shared_ptr<ZeroNode>, MoveHash> children;
+    std::unordered_map<Move, Branch, MoveHash> branches;
+    float value;
+    int total_visit_count = 1;
+    bool terminal;
+
+    ZeroNode(const board::Board& game_board, float value,
+             std::unordered_map<Move, float, MoveHash> priors,
+             std::weak_ptr<ZeroNode> parent,
+             bool add_noise);
+
+    void add_child(Move move, std::shared_ptr<ZeroNode> child) {
+      children.emplace(move, child);
+    }
+
+    void record_visit(Move m, float val);
+
+    float expected_value(Move m) const;
+
+    float prior(Move m) const {
+      return branches.find(m)->second.prior;
+    }
+
+    int visit_count(Move m) const {
+      auto it = branches.find(m);
+      if (it != branches.end())
+        return it->second.visit_count;
+      return 0;
+    }
+  };
+
+
+  class ZeroAgent : public Agent {
+    torch::jit::script::Module model;
+    std::shared_ptr<Encoder> encoder;
+    int num_rounds;
+    float c_uct;
+
+    // std::shared_ptr<ExperienceCollector> collector;
+
+    // If True, always select moves that maximize visit count.  Otherwise, initial
+    // moves are selected in proportion to visit count.
+    bool greedy;
+
+  public:
+    ZeroAgent(torch::jit::script::Module model,
+              // std::shared_ptr<Encoder> encoder,
+              int num_rounds,
+              bool greedy = true,
+              float c_uct = 1.5) :
+      model(model), num_rounds(num_rounds), c_uct(c_uct), greedy(greedy) {}
+
+    Move select_move(const board::Board&);
+
+    // void set_collector(std::shared_ptr<ExperienceCollector> c) {
+    //   collector = c;
+    // }
+
+  private:
+    std::shared_ptr<ZeroNode> create_node(const board::Board& b,
+                                          std::optional<Move> move = std::nullopt,
+                                          std::weak_ptr<ZeroNode> parent = std::weak_ptr<ZeroNode>());
+    Move select_branch(const ZeroNode& node) const;
+  };
+
+};
+
+
+#endif // AGENT_ZERO_H
