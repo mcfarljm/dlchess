9ffd9cb38c17fc910226a5fb9707685d2a94159c
John McFarland <mcfarljm@gmail.com>
2023-11-21 11:08:32 -0700

    Add training script

---
+++ train/data.py
@@ -0,0 +1,90 @@
+import os
+import glob
+import json
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset, ConcatDataset, Subset
+
+
+DATATYPES = {'float32': np.float32,
+             'int8': np.int8,
+             'int16': np.int16}
+
+
+class ExperienceSubset(Subset):
+    """Random subset of items from all experience files in a directory."""
+    def __init__(self, directory, n):
+        dataset = ExperienceSet(directory)
+        print(f'loaded experience data with {len(dataset)} moves')
+        if n <= 1:
+            # Treat as fraction
+            n = int(len(dataset) * n)
+        if n > len(dataset):
+            raise ValueError
+        print(f'using subset of {n} moves')
+        indices = np.random.choice(len(dataset), n, replace=False)
+        super().__init__(dataset, indices)
+
+
+class ExperienceSet(ConcatDataset):
+    """Concatenate all experience files in a given directory."""
+    def __init__(self, directory):
+        labels = [s.split('states')[1].split('.json')[0] for s in
+                  glob.glob(os.path.join(directory, 'states*.json'))]
+        triples = [[os.path.join(directory, f'{key}{label}.json')
+                    for key in ('states', 'rewards', 'visit_counts')]
+                   for label in labels]
+        datasets = [ExperienceChunk(*triple) for triple in triples]
+        super().__init__(datasets)
+
+
+class ExperienceChunk(Dataset):
+    """Experience data for an individual item.
+
+    Intent is for these to be collected using ChainDataSet.
+    """
+    def __init__(self, states_path, rewards_path, visit_counts_path):
+        """Args:
+        path (str): Path to states json file.
+        """
+        self.states_path = states_path
+        self.rewards_path = rewards_path
+        self.visit_counts_path = visit_counts_path
+        self.dirname = os.path.dirname(states_path)
+        assert self.dirname == os.path.dirname(rewards_path) == os.path.dirname(visit_counts_path)
+
+        with open(states_path, 'r') as f:
+            self.states_info = json.load(f)
+        with open(rewards_path, 'r') as f:
+            self.rewards_info = json.load(f)
+        with open(visit_counts_path, 'r') as f:
+            self.visit_counts_info = json.load(f)
+
+        self.num_moves = self.states_info['shape'][0]
+        # print('loading chunk with', self.num_moves, 'moves')
+        strides = self.states_info['strides']
+        assert strides[0] > strides[1] > strides[2] > strides[3]
+
+    def _memmap_data(self, info):
+        return np.memmap(os.path.join(self.dirname, info['data']),
+                         dtype=DATATYPES[info['dtype']],
+                         mode='r',
+                         shape=tuple(info['shape']))
+
+    def __len__(self):
+        return self.num_moves
+
+    def __getitem__(self, idx):
+
+        memmaps = [self._memmap_data(info) for info in (
+            self.states_info, self.rewards_info, self.visit_counts_info)]
+
+        return [torch.tensor(m[idx]) for m in memmaps]
+
+
+if __name__ == '__main__':
+    # data = ExperienceChunk(*[f'example/{k}_1.json' for k in ('states', 'rewards', 'visit_counts')])
+    # data_aug = AugmentedExperienceChunk(*[f'example/{k}_1.json' for k in ('states', 'rewards', 'visit_counts')])
+    data = ExperienceSet('experience')
+    # data = ExperienceSubset('example', 5)
---
+++ train/train.py
@@ -0,0 +1,95 @@
+import os
+
+import torch
+from torch.utils.data import DataLoader
+import click
+
+from data import ExperienceSubset
+
+
+def cross_entropy_loss_fn(policies, visit_counts):
+    search_probs = visit_counts / visit_counts.sum((1, 2, 3), keepdims=True)
+    assert search_probs[0].sum() > 0.999 and search_probs[0].sum() < 1.001
+
+    # We divide by the number of rows (examples) to get a mean of the
+    # cross-entropy loss.
+    return -(search_probs * torch.log(policies)).sum() / len(search_probs)
+
+
+def train(dataloader, model, optimizer, output_interval):
+    mse_loss_fn = torch.nn.MSELoss()
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.train()
+    for batch_num, (states, rewards, visit_counts) in enumerate(dataloader):
+
+        # Compute prediction error
+        policies, values = model(states)
+
+        mse_loss = mse_loss_fn(values.squeeze(), rewards)
+        cross_entropy_loss = cross_entropy_loss_fn(policies, visit_counts)
+
+        loss = mse_loss + cross_entropy_loss
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch_num % output_interval == 0:
+            loss, current = loss.item(), (batch_num + 1) * len(states)
+            mse_loss, ce_loss = mse_loss.item(), cross_entropy_loss.item()
+            print(f"loss: {loss:>7f} ({mse_loss:>3f}, {ce_loss:>3f})  [{current:>5d}/{size:>5d}] [{batch_num + 1}/{num_batches}]")
+
+
+@click.command()
+@click.option('-e', '--experience', required=True)
+@click.option('-q', '--query', is_flag=True, help='query experience')
+@click.option('-b', '--batch-size', type=int, default=256)
+@click.option('-i', '--input-path', help='path to input parameter file')
+@click.option('-o', '--output-path')
+@click.option('-n', '--subset', default=1.0, help='number or fraction of examples to use')
+@click.option('--lr', default=1e-2, help='learning rate')
+@click.option('--interval', default=1, help='output interval')
+@click.option('-f', '--force', is_flag=True, help='overwrite existing output files')
+def main(experience, query, batch_size, input_path, output_path, subset, lr, interval, force):
+    THIS_DIR = os.path.abspath(os.path.dirname(__file__))
+
+    import sys
+    sys.path.append(os.path.join(THIS_DIR, '../nn'))
+    from conv_4x64 import ChessNet
+
+    if int(subset) == subset:
+        subset = int(subset)
+
+    if output_path and (not force) and os.path.exists(output_path):
+        raise ValueError('output path exists')
+
+    dataset = ExperienceSubset(experience, subset)
+    dataloader = DataLoader(dataset, batch_size)
+    if query:
+        return
+    
+    model = ChessNet()
+    model.load_state_dict(torch.load(input_path))
+    
+    optimizer = torch.optim.SGD(model.parameters(),
+                                # Paper starts with 1e-2
+                                lr=lr,
+                                momentum=0.9,
+                                weight_decay=1e-4)
+
+    train(dataloader, model, optimizer, interval)
+
+    if output_path:
+        torch.save(model.state_dict(), output_path)
+
+        # Torchscript
+        model.eval()
+        X = torch.rand(1, encoder_channels, grid_size, grid_size)
+        traced_script_module = torch.jit.trace(model, X)
+        traced_script_module.save(output_path.replace('.pt', '.ts'))
+
+
+if __name__ == '__main__':
+    main()
