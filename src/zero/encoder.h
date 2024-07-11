2f8475296084ff1e5d1e02ff2f463ea55096659b
John McFarland <mcfarljm@gmail.com>
2024-07-10 19:22:31 -0600

    Add en_passant option to SimpleEncoder
    This adds an en passant plane to the network input encoding.  It is activated by passing
    a version>0 to the SimpleEncoder constructor.

--- src/zero/encoder.h
+++ src/zero/encoder.h
@@ -17,7 +17,9 @@ namespace zero {
   };
 
   class SimpleEncoder : public Encoder {
+    bool en_passant_;
   public:
+    SimpleEncoder(int version=0) : en_passant_{version>0} {}
     Tensor<float> encode(const board::Board&) const;
     /* Move decode_move_index(int index) const; */
   };
