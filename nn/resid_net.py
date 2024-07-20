e22af6f55c5df39e820b535f22996e7aa2517e3b
John McFarland <mcfarljm@gmail.com>
2024-07-20 06:33:45 -0600

    Add --encoding-version option to resid_net

--- nn/resid_net.py
+++ nn/resid_net.py
@@ -105,7 +105,8 @@ def count_parameters(model):
 @click.option("-o", "--output", default="resid_4x64.pt")
 @click.option("-f", "--force", is_flag=True, help="overwrite")
 @click.option("-i", "--input", help="input file with model state")
-def main(output, force, input):
+@click.option('-v', '--encoding-version', default=1, show_default=True)
+def main(output, force, input, encoding_version):
     if not output.endswith(".pt"):
         output += ".pt"
     if not force:
@@ -117,7 +118,7 @@ def main(output, force, input):
             raise ValueError(".onnx output exists")
 
     grid_size = 8
-    encoder_channels = 21
+    encoder_channels = 21 if encoding_version == 0 else 22
     model = ChessNet(in_channels=encoder_channels)
     if input is not None:
         model.load_state_dict(torch.load(input))
