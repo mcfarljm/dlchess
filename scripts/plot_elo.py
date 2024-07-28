c1a3f69e742bdb961e60f9cfc13b49f15ec52b67
John McFarland <mcfarljm@gmail.com>
2024-07-28 16:24:02 -0600

    Updates to plot_elo.py script
    Handle 100% win rate and add --dark option.

--- scripts/plot_elo.py
+++ scripts/plot_elo.py
@@ -5,19 +5,30 @@ import click
 import matplotlib.pyplot as plt
 
 
-def parse_eval_file(path):
+def parse_eval_file(path, inf_val=600.0):
+    """Parse chutechess output and return elo difference.
+
+    Parameters
+    ----------
+    inf_val:
+        Elo difference to use when side scored 100%.
+    """
     with open(path, 'r') as f:
         for line in f:
             if line.startswith('Elo'):
-                return float(line.split()[2])
+                val = float(line.split()[2])
+                if val == float("inf"):
+                    return inf_val
+                return val
     raise ValueError
 
 
 @click.command
 @click.argument('pattern')
 @click.option('-g', '--games-per', default=3200, help='selfplay games per iteration')
+@click.option('-d', '--dark', is_flag=True, help='dark background')
 @click.option('-s', '--save', is_flag=True)
-def main(pattern, games_per, save):
+def main(pattern, games_per, dark, save):
     """Plot ELO vs games by parsing cutechess-cli eval output.
 
     PATTERN is a shell glob pattern used to identify a set of evaluation output files produced by cutechess-cli.
@@ -33,10 +44,13 @@ def main(pattern, games_per, save):
 
     xvals = list(range(0, games_per * len(elo_diffs) + 1, games_per))
 
+    if dark:
+        plt.style.use('dark_background')
+
     f, ax = plt.subplots()
     ax.plot(xvals, elos, '-o')
     ax.set_xlabel('Selfplay games')
-    ax.set_ylabel('ELO')
+    ax.set_ylabel('Relative ELO')
     ax.grid()
 
     if save:
