5b147ea6d1b1be878d8da4ca2075f7d63227cc52
John McFarland <mcfarljm@gmail.com>
2024-08-03 12:23:22 -0600

    Add chess/transform.h
    Move bitboard transformation code from encoder.cpp to chess/transform.h.  This is in
    anticipation of adding additional transform functions that operate on squares or moves.

--- src/zero/encoder.cpp
+++ src/zero/encoder.cpp
@@ -1,58 +1,23 @@
 #include <bitset>
 
+#include "../chess/transform.h"
 #include "encoder.h"
 
 using chess::GRID_SIZE;
 
 namespace {
-  enum {
-    // Horizontal mirror, reverse_bits_in_bytes
-    flip_transform,
-    // Vertical mirror, reverse_bytes_in_bytes
-    mirror_transform,
-    // Diagonal tranpose A1 to H8, tranpose_bits_in_bytes
-    tranpose_transform,
-  };
 
-  // Flip bitboard horizontally.
-  inline uint64_t reverse_bits_in_bytes(uint64_t v) {
-    v = ((v >> 1) & 0x5555555555555555ull) | ((v & 0x5555555555555555ull) << 1);
-    v = ((v >> 2) & 0x3333333333333333ull) | ((v & 0x3333333333333333ull) << 2);
-    v = ((v >> 4) & 0x0F0F0F0F0F0F0F0Full) | ((v & 0x0F0F0F0F0F0F0F0Full) << 4);
-    return v;
-  }
-
-  // Flip bitboard vertically.
-  inline uint64_t reverse_bytes_in_bytes(uint64_t v) {
-    v = (v & 0x00000000FFFFFFFF) << 32 | (v & 0xFFFFFFFF00000000) >> 32;
-    v = (v & 0x0000FFFF0000FFFF) << 16 | (v & 0xFFFF0000FFFF0000) >> 16;
-    v = (v & 0x00FF00FF00FF00FF) << 8 | (v & 0xFF00FF00FF00FF00) >> 8;
-    return v;
-  }
-
-  using Transform = std::bitset<3>;
+  using chess::Transform;
+  using chess::TransformType;
 
   Transform choose_transform(const chess::Board& b) {
     Transform transform;
     if (b.side == chess::Color::black) {
       // Orient board towards side to move by performing vertical and horizontal flip.
-      transform.set(flip_transform);
-      transform.set(mirror_transform);
+      transform.set(static_cast<int>(chess::TransformType::flip_transform));
+      transform.set(static_cast<int>(chess::TransformType::mirror_transform));
     }
     return transform;
-  }
-
-  chess::Bitboard transform_bitboard(chess::Bitboard bb, Transform transform) {
-    if (! transform.any() || ! bb.any())
-      return bb;
-
-    auto new_bb_ul = bb.to_ullong();
-    if (transform[flip_transform])
-      new_bb_ul = reverse_bits_in_bytes(new_bb_ul);
-    if (transform[mirror_transform])
-      new_bb_ul = reverse_bytes_in_bytes(new_bb_ul);
-
-    return chess::Bitboard {new_bb_ul};
   }
 
 };
@@ -74,7 +39,7 @@ namespace zero {
     for (int piece_idx=0; piece_idx<chess::NUM_PIECE_TYPES_BOTH; ++piece_idx) {
       auto bb = b.bitboards[static_cast<int>(piece_idx)];
       if (have_transform)
-        bb = transform_bitboard(bb, transform);
+        bb = chess::transform_bitboard(bb, transform);
       for (auto sq : bb) {
         auto coords = chess::sq_to_rf(sq);
         board_tensor.at({0, piece_idx, coords[0], coords[1]}) = 1.0;
