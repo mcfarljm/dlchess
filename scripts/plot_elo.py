2003c963150104fcc4bee5cccd3a826bc723ec54
John McFarland <mcfarljm@gmail.com>
2024-08-11 16:39:13 -0600

    Add --cumulative flag
    Intended for use in plotting reference evaluations.

--- scripts/plot_elo.py
+++ scripts/plot_elo.py
@@ -28,21 +28,25 @@ def parse_eval_file(path, inf_val=600.0):
 @click.option('-g', '--games-per', default=3200, help='selfplay games per iteration')
 @click.option('-d', '--dark', is_flag=True, help='dark background')
 @click.option('-s', '--save', is_flag=True)
-def main(pattern, games_per, dark, save):
+@click.option('--cumulative/--no-cumulative', is_flag=True, default=True)
+def main(pattern, games_per, dark, save, cumulative):
     """Plot ELO vs games by parsing cutechess-cli eval output.
 
     PATTERN is a shell glob pattern used to identify a set of evaluation output files produced by cutechess-cli.
 
     """
     paths = glob.glob(pattern)
-    paths = sorted(paths, key=lambda x: int(x.split('_')[1].split('.')[1]))
+    paths = sorted(paths, key=lambda x: int(x.split('.')[-2]))
     # print(paths)
     elo_diffs = [parse_eval_file(p) for p in paths]
     print(elo_diffs)
-    elos = list(accumulate(elo_diffs))
-    elos.insert(0, 0)
-
-    xvals = list(range(0, games_per * len(elo_diffs) + 1, games_per))
+    if cumulative:
+        elos = list(accumulate(elo_diffs))
+        elos.insert(0, 0)
+        xvals = list(range(0, games_per * len(elo_diffs) + 1, games_per))
+    else:
+        elos = elo_diffs
+        xvals = list(range(games_per, games_per * len(elos) + 1, games_per))
 
     if dark:
         plt.style.use('dark_background')
